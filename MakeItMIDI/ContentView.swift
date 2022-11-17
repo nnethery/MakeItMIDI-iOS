ac0d17c8a8f3a28368eaf1ae424c6a8dd746f8dd
Noah Nethery <nethery.n@gmail.com>
2022-11-17 17:31:10 -0500

    Initial Commit

---
+++ MakeItMIDI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MakeItMIDI
+//
+//  Created by Noah Nethery on 11/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MakeItMIDI/MakeItMIDIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeItMIDIApp.swift
+//  MakeItMIDI
+//
+//  Created by Noah Nethery on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct MakeItMIDIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
